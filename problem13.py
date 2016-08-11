0e936605a1069cceebdcd2ca7cde822c9e635fbe
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2016-08-10 22:22:43 -0700

    solve modified version of problem 13

--- problem13.py
+++ problem13.py
@@ -1,3 +1,11 @@
-# Work out the first ten digits of the sum of the
+# Work out the last ten digits of the sum of the
 # following one-hundred 50-digit numbers.
 
+from bignum import bignum
+
+result = 0
+numlist = bignum.split()
+for num in numlist:
+	result += int(num[-10:])
+
+print str(result)[-10:]
