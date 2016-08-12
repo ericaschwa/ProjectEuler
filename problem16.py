8b520d8148f92f8fca30b2b87971b247f19d1786
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2016-08-11 22:28:58 -0700

    finished problem16

--- problem16.py
+++ problem16.py
@@ -1,3 +1,14 @@
 # 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
 # What is the sum of the digits of the number 2^1000?
+
+def digitsInBigNum(n, base):
+	digits = []
+	while n > 0:
+		digits.append(n%base)
+		n /= base
+	return digits
+
+bignumdigits = digitsInBigNum(pow(2,15), 10)
+print sum(bignumdigits)
+
