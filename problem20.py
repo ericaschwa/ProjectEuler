d3bbe77de4023bb41337715fa4c11d7c48a73f83
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2016-08-14 14:45:49 -0700

    finish problem 20

--- problem20.py
+++ problem20.py
@@ -1,7 +1,20 @@
-# n! means n x (n − 1) x ... x 3 x 2 x 1
-
-# For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+# 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
 # and the sum of the digits in the number 10! is
 # 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 # Find the sum of the digits in the number 100!
+
+import math
+
+
+def sumFact(n):
+	num = math.factorial(n)
+	result = 0
+	while num > 0:
+		result += num % 10
+		num = num / 10
+	return result
+
+print sumFact(10) # 27
+print sumFact(100) # 648
+
