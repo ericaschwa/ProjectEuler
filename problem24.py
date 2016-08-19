b1e6336bcafaf5f32d05d0c62ef7849f273fc826
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2016-08-18 23:24:52 -0400

    solve problem 24

--- problem24.py
+++ problem24.py
@@ -8,3 +8,16 @@
 # What is the millionth lexicographic permutation of the digits
 # 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
+permutations = []
+
+def findPermutations(head, tail):
+	if len(tail) == 0:
+		permutations.append(head)
+	for i in range(0, len(tail)):
+		findPermutations(head + tail[i], tail[:i] + tail[i+1:])
+
+findPermutations("", ["0", "1", "2"])
+print permutations # ['012', '021', '102', '120', '201', '210']
+
+findPermutations("", ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+print permutations[1000000] # 2783914605
